2b0b841ee5231699ed8a34f30109c4707aa9d5c5
blue-reino <104793927+blue-reino@users.noreply.github.com>
2024-02-05 22:07:23 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
 import logo from './logo.svg';
 import './App.css';
+import './Header.css'
+import './Home.css'
+import movieImages from './movie-images';
+
+
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const Header = () => {
+    return (
+
+      <header className="header">
+
+        <div className="header_Nav">
+
+          <div className="header_logo">
+            NETFLIX
+          </div>{
+
+          }
+          <a href="#">Home</a>
+          <a href="#">TV Shows</a>
+          <a href="#">Movies</a>
+          <a href="#">New & Popular</a>
+          <a href="#">My List</a>
+          <a href="#">Browse by Languages</a>
+        </div>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+
       </header>
+
+    );
+  }
+
+  const Billboard = () => {
+
+    return (
+      <div className="billboard-container">
+        <div className="item billboard bbmovie"></div>
+
+
+      </div>
+
+
+    );
+  }
+  const Mainview = () => {
+
+    const [width, setWidth] = useState(0);
+    const movieSlider = useRef();
+    useEffect(() => {
+      setWidth(1500);
+    }, []);
+
+
+    return (
+      <div className="content-container">
+        <div className="section-title">
+          <h2>New Releases</h2>
+
+          <motion.div ref={movieSlider} className="movie-scroller" whileTap={{ cursor: "grabbing" }}>
+
+
+            <motion.div drag="x" dragConstraints={{right: 0, left: -width}} className="movie-card-1">
+              {movieImages.map(image => {
+                return (
+                  <motion.div className="poster" key={image}>
+                    <img src={image} alt="movie-image" />
+                  </motion.div>
+                );
+              })}
+            </motion.div>
+
+
+
+
+
+
+          </motion.div>
+
+        </div>
+
+        <div className="section-title">
+          <h2>Top 10 Movies in the U.S Today</h2>
+          <div className="movie-card-2">
+            <div className="movie-image"></div>
+          </div>
+        </div>
+
+        <div className="section-title">
+          <h2>Only on Netflix</h2>
+          <div className="movie-card-3">
+            <div className="movie-image"></div>
+          </div>
+        </div>
+
+        <div className="section-title">
+          <h2>Trending Now</h2>
+          <div className="movie-card-4">
+            <div className="movie-image"></div>
+          </div>
+        </div>
+
+
+
+      </div>
+    );
+  }
+
+
+
+  return (
+
+    <div className="App">
+      <Header />
+      <Billboard />
+      <Mainview />
+
     </div>
+
+
   );
 }
+
+
 
 export default App;
---
+++ src/movie-images.js
@@ -0,0 +1,19 @@
+import movie1 from './images/scream.png';
+import movie2 from './images/scream2.png';   
+import movie3 from './images/shamar.png';   
+import movie4 from './images/avengers.jpg';   
+import movie5 from './images/cars.png';   
+import movie6 from './images/dune.png';
+import movie7 from './images/fordvferrari.png'; 
+import movie8 from './images/interstellar.png'; 
+import movie9 from './images/oppenheimer.jpg'; 
+import movie10 from './images/transformers.jpg';
+import movie11 from './images/shamardad.png';
+import movie12 from './images/shamarmom.png';
+
+
+
+
+
+
+export default [movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9, movie10, movie11, movie12];
